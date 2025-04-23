aac30dff4c455f61e418220b9fe50d5df2713f67
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-04-23 18:09:21 +0000

    Added conditional logic

--- app/page.js
+++ app/page.js
@@ -4,15 +4,33 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-  const [thumbs, setThumbs] = useState("")
+  const [thumbs, setThumbs] = useState(null)
+
+  const handleClick = () => {
+    //
+    const randomNumber = Math.random();
+    let outcome;
+
+    if (randomNumber < 0.5) {
+      outcome = "up";
+    } else {
+      outcome = "down";
+    }
+
+    setThumbs(outcome);
+  }
+
+  const getImage = () => {
+    //
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
        <h1 className="font-bold text-xl">Do you deserve a thumbs up or thumbs down?</h1>
-        <form onSubmit="" className="flex gap-4 items-center flex-col sm:flex-row">
           <button
+          onClick={handleClick}
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            type="submit"
           >
             <Image
               className="dark:invert"
@@ -23,7 +41,10 @@ export default function Home() {
             />
             Find out
           </button>
-        </form>
+
+          {thumbs && (
+            <p>{thumbs === "up" ? "Thumbs up" : "Thumbs down" }</p>
+          )}
       </main>
     </div>
   );
