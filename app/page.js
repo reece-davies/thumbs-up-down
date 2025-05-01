3ba43ba1c97b92c7af38f6bdce7beeb889378336
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-05-01 18:41:16 +0000

    Added thumbs up and down images from icons8

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     const randomNumber = Math.random();
     let outcome;
 
-    if (randomNumber < 0.5) {
+    if (randomNumber < 0.6) {
       outcome = "up";
     } else {
       outcome = "down";
@@ -21,19 +21,22 @@ export default function Home() {
   }
 
   const getImage = () => {
-    //
+    // Up
+    if (thumbs === "up") return "https://img.icons8.com/color/96/thumb-up--v1.png";
+
+    // Down
+    if (thumbs === "down") return "https://img.icons8.com/flat-round/64/thumbs-down--v1.png";
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
        <h1 className="font-bold text-xl">Do you deserve a thumbs up or thumbs down?</h1>
           <button
-          onClick={handleClick}
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            onClick={handleClick}
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
           >
             <Image
-              className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
@@ -43,7 +46,14 @@ export default function Home() {
           </button>
 
           {thumbs && (
-            <p>{thumbs === "up" ? "Thumbs up" : "Thumbs down" }</p>
+            <div className="flex flex-col justify-center items-center w-full">
+              <p className="m-3">{thumbs === "up" ? "Thumbs up" : "Thumbs down" }</p>
+              <img
+                src={getImage()}
+                alt={`Thumbs ${thumbs}`}
+                width={100}
+                height={100} />
+            </div>
           )}
       </main>
     </div>
